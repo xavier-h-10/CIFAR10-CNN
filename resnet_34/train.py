87f8ae7f67a25c94d8c4f3dd8774a62e038ee1cb
okabe <923048992@qq.com>
2021-12-25 16:48:14 +0800

    amplify dataset

--- resnet_34/train.py
+++ resnet_34/train.py
@@ -41,9 +41,26 @@ BATCH_SIZE = 128  # 批处理尺寸(batch_size)
 LR = 0.1  # 学习率
 
 # 准备数据集并预处理
-transform_train = transforms.Compose([
+transform_train_amplified1 = transforms.Compose([
     transforms.RandomCrop(32, padding=4),  # 先四周填充0，再把图像随机裁剪成32*32
     transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),  # R,G,B每层的归一化用到的均值和方差
+])
+
+transform_train_amplified2 = transforms.Compose([
+    transforms.RandomAffine(degrees=20),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),  # R,G,B每层的归一化用到的均值和方差
+])
+
+transform_train_amplified3 = transforms.Compose([
+    transforms.ColorJitter(brightness=0.2, saturation=0.1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),  # R,G,B每层的归一化用到的均值和方差
+])
+
+transform_train = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),  # R,G,B每层的归一化用到的均值和方差
 ])
@@ -52,8 +69,19 @@ transform_test = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
-
+trainset_amplied1 = torchvision.datasets.CIFAR10(root='./data', train=True, download=True,
+                                                 transform=transform_train_amplified1)
+trainset_amplied2 = torchvision.datasets.CIFAR10(root='./data', train=True, download=True,
+                                                 transform=transform_train_amplified2)
+trainset_amplied3 = torchvision.datasets.CIFAR10(root='./data', train=True, download=True,
+                                                 transform=transform_train_amplified3)
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)  # 训练数据集
+print(trainset.data.shape)
+
+trainset = trainset.__add__(trainset_amplied1)
+trainset = trainset.__add__(trainset_amplied2)
+trainset = trainset.__add__(trainset_amplied3)
+
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True,
                                           num_workers=2)  # 生成一个个batch进行批训练，组成batch的时候顺序打乱取
 
@@ -65,11 +93,12 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 # 模型定义-ResNet
 # net = ResNet18().to(device)
 
-net=ResNet18().to(device)
+net = ResNet18().to(device)
 
 # 定义损失函数和优化方式
 criterion = nn.CrossEntropyLoss()  # 损失函数为交叉熵，多用于多分类问题
-optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9, weight_decay=5e-4) #优化方式为mini-batch momentum-SGD，并采用L2正则化（权重衰减）
+optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9,
+                      weight_decay=5e-4)  # 优化方式为mini-batch momentum-SGD，并采用L2正则化（权重衰减）
 scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 # optimizer = optim.Adam(net.parameters(), lr=LR, betas=(0.9, 0.99), weight_decay=5e-4)
 
