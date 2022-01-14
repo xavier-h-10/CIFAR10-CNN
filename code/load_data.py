0ae0d25f7fd7658a573a00b477f292625f613a6b
Tommy Huang <13917078913@163.com>
2022-01-14 19:46:19 +0800

    update position

---
+++ code/load_data.py
@@ -0,0 +1,55 @@
+import platform
+from six.moves import cPickle as pickle
+
+# By default, please use python 3.x to run our Project
+def load_pickle(f):
+    return pickle.load(f, encoding='latin1')
+
+
+# Load one data batch
+def load_CIFAR_batch(filename):
+    with open(filename, 'rb') as f:
+        datadict = load_pickle(f)
+        X = datadict['data']
+        Y = datadict['labels']
+        X = X.reshape(10000, 32, 32, 3)
+        Y = np.array(Y)
+        return X, Y
+
+
+# load all 6 data batch
+def load_CIFAR10(ROOT):
+    xs = []
+    ys = []
+    for b in range(1, 6):
+        f = os.path.join(ROOT, 'data_batch_%d' % (b,))
+        X, Y = load_CIFAR_batch(f)
+        xs.append(X)
+        ys.append(Y)
+    Xtr = np.concatenate(xs)
+    Ytr = np.concatenate(ys)
+    del X, Y
+    Xte, Yte = load_CIFAR_batch(os.path.join(ROOT, 'test_batch'))
+    return Xtr, Ytr, Xte, Yte
+
+
+# by default, train data: test data = 5:1; No validation data
+def get_CIFAR10_data(num_training=50000, num_test=10000):
+    cifar10_dir = 'cifar-10-batches-py/'
+    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+    # Subsample the data
+    mask = range(num_training)
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+    mask = range(num_test)
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+
+    x_train = X_train.astype('float32')
+    x_test = X_test.astype('float32')
+
+    x_train /= 255
+    x_test /= 255
+
+    return (x_train, y_train), (x_test, y_test)
---
+++ code/official_example/detect.py
@@ -0,0 +1,38 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+from official_example.model import Net
+
+batch_size = 4
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+testset = torchvision.datasets.CIFAR10(root='./resnet_34/data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=0)
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+PATH = 'cifar_net.pth'
+net = Net()
+net.load_state_dict(torch.load(PATH))
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
